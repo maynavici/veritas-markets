84620ad3d2915a46e64281b0a2601dc1f3ebb8c6
maynavici <152253581+maynavici@users.noreply.github.com>
2026-06-09 22:48:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Paste your Supabase Project URL and anon public key here.
 // NEVER paste your service_role key in frontend code.
 
-window.VERITAS_SUPABASE_URL = "https://mpwpjsptlwwksahehqjb.supabase.co/rest/v1/";
+window.VERITAS_SUPABASE_URL = "https://mpwpjsptlwwksahehqjb.supabase.co";
 window.VERITAS_SUPABASE_ANON_KEY = "sb_publishable_rKeoHrba6qvG9aLk3cmH7A_DlGhGrE9";
