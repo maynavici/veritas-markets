a1dbb3a7a964f4bcd7c8969cde9468241444dbc8
maynavici <152253581+maynavici@users.noreply.github.com>
2026-06-09 22:39:18 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// VERITAS SUPABASE CONFIG
+// Paste your Supabase Project URL and anon public key here.
+// NEVER paste your service_role key in frontend code.
+
+window.VERITAS_SUPABASE_URL = "https://mpwpjsptlwwksahehqjb.supabase.co/rest/v1/";
+window.VERITAS_SUPABASE_ANON_KEY = "sb_publishable_rKeoHrba6qvG9aLk3cmH7A_DlGhGrE9";
